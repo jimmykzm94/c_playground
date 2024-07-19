7c560aa2911508f2b8e7de70e25ae34cb03749a5
Jimmy Kueh <jimmykzm94@gmail.com>
2024-07-20 02:08:11 +0800

    Format posix code

--- serial_port/serial_posix.c
+++ serial_port/serial_posix.c
@@ -5,9 +5,10 @@
 #include <unistd.h>
 #include <termios.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     // Check argument
-    if(argc < 2)
+    if (argc < 2)
     {
         printf("No serial port found\n");
         return 1;
@@ -18,7 +19,8 @@ int main(int argc, char *argv[]){
 
     // Open serial port
     serial_port = open(argv[1], O_RDONLY);
-    if(serial_port < 0){
+    if (serial_port < 0)
+    {
         perror("Failed to open serial port");
         return 1;
     }
@@ -26,7 +28,8 @@ int main(int argc, char *argv[]){
     // Configure serial port
     struct termios tty;
     memset(&tty, 0, sizeof(tty));
-    if(tcgetattr(serial_port, &tty) != 0){
+    if (tcgetattr(serial_port, &tty) != 0)
+    {
         perror("Error from tcgetattr");
         close(serial_port);
         return 1;
@@ -37,11 +40,11 @@ int main(int argc, char *argv[]){
     cfsetispeed(&tty, B9600);
 
     // Set other serial port parameters
-    tty.c_cflag &= ~PARENB;  // No parity
-    tty.c_cflag &= ~CSTOPB;  // 1 stop bit
-    tty.c_cflag &= ~CSIZE;   // Clear data size bits
-    tty.c_cflag |= CS8;      // 8 bits per byte
-    tty.c_cflag &= ~CRTSCTS; // Disable hardware flow control
+    tty.c_cflag &= ~PARENB;        // No parity
+    tty.c_cflag &= ~CSTOPB;        // 1 stop bit
+    tty.c_cflag &= ~CSIZE;         // Clear data size bits
+    tty.c_cflag |= CS8;            // 8 bits per byte
+    tty.c_cflag &= ~CRTSCTS;       // Disable hardware flow control
     tty.c_cflag |= CREAD | CLOCAL; // Turn on receiver and ignore control lines
 
     // Set input mode (non-canonical, no echo)
@@ -54,7 +57,8 @@ int main(int argc, char *argv[]){
     tty.c_cc[VTIME] = 1;
 
     // Apply settings
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0){
+    if (tcsetattr(serial_port, TCSANOW, &tty) != 0)
+    {
         perror("Error from tcsetattr");
         close(serial_port);
         return 1;
@@ -63,25 +67,32 @@ int main(int argc, char *argv[]){
     // Read from serial port
     int buffer_index;
     char recv;
-    while(1){
+    while (1)
+    {
         memset(&recv, 0, sizeof(recv));
         int num_bytes = read(serial_port, &recv, 1);
-        if(num_bytes < 0){
+        if (num_bytes < 0)
+        {
             perror("Error reading from serial port");
             close(serial_port);
             return 1;
         }
-        else if(num_bytes > 0){
-            if(recv == '\n' ){
+        else if (num_bytes > 0)
+        {
+            if (recv == '\n')
+            {
                 buffer[buffer_index] = '\0';
                 printf("Received: %s\n", buffer);
                 buffer_index = 0;
 
                 // If done message, break
-                if(strncmp(buffer, "Done", 4) == 0){
+                if (strncmp(buffer, "Done", 4) == 0)
+                {
                     break;
                 }
-            } else {
+            }
+            else
+            {
                 buffer[buffer_index++] = recv;
             }
         }
